6aab58cf6185bf5e48017ebf6fb286f038adf93d
Nguyen Gia Han <106945180+hannibaI2023@users.noreply.github.com>
2022-06-06 17:58:19 +0700

    newfiles

---
+++ animation.js
@@ -0,0 +1,75 @@
+function calheight() {
+   var x = window.innerHeight;
+   return x - 80;
+}
+
+$('#terminal').terminal({
+   help: function(){
+      this.echo(`Available commands: about, banner, clear, date, echo, facebook, github, help, mail, projects\nFind the hidden command to experience magic.`)
+   },
+   about: function(){
+      this.echo(`I am a high school student at VNU High school for the Gifted.\nWith a passion for the world of computers, I aspire to work in the field of cybersecurity and beyond.`)
+   },
+   banner: function(){
+      this.echo(greetings.innerHTML)
+   },
+   clear: function(){
+      clear();
+   },
+   date: function(){
+      const d = new Date();
+      this.echo(d);
+   },
+   echo: function(str){
+      this.echo(str)
+   },
+   projects: function(){
+      var calc = $('<a href="https://github.com/hannibaI2023/Calculator">https://github.com/hannibaI2023/Calculator</a>');
+      var spek = $('<a href="https://github.com/hannibaI2023/Senku-Speaker">https://github.com/hannibaI2023/Senku-Speaker</a>');
+      var lich = $('<a href="https://github.com/hannibaI2023/LichVanNien">https://github.com/hannibaI2023/LichVanNien</a>');
+      var sher = $('<a href="https://drive.google.com/drive/folders/1yWEW5rkrtARuSfyiK3wN1ZwSjGr_A_eD?usp=sharing">https://drive.google.com/drive/folders/1yWEW5rkrtARuSfyiK3wN1ZwSjGr_A_eD?usp=sharing</a>');
+      this.echo("Maho calculator - a simple calculator: "); this.echo(calc);
+      this.echo("Senku speaker - an app that reads numbers: "); this.echo(spek);
+      this.echo("Lich Van Nien - a calendar: "); this.echo(lich);
+      this.echo("SHErlock - a short puzzle game (art by Hiu Kao, idea by Bao Ngoc): "); this.echo(sher);
+   },
+   github: function(){
+      var git = $('<a href="https://github.com/hannibaI2023">hannibaI2023</a>');
+      this.echo(git);
+   },
+   facebook: function(){
+      var face = $('<a href="https://www.facebook.com/han.nibal2023/">han_nibal2023</a>');
+      this.echo(face);
+   },
+   mail: function(){
+      this.echo("Please mail to: poachedeggg6@gmail.com")
+   },
+   rickroll: function(){
+      this.echo(`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⠿⠟⠛⠻⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣆⣀⣀⠀⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠻⣿⣿⣿⠅⠛⠋⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢼⣿⣿⣿⣃⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣟⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⣛⣫⡄⠀⢸⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡆⠸⣿⣿⣿⡷⠂⠨⣿⣿⣿⣿⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⡇⢀⣿⡿⠋⠁⢀⡶⠪⣉⢸⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⣷⣿⣿⣷⣦⡙⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`);
+   window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+   }
+}, {
+      greetings: greetings.innerHTML,
+      prompt: "C:\\Users\\hanng>",
+      height: calheight(),
+      checkArity: false
+})
+
+
